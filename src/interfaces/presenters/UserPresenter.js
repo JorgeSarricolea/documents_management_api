a6f2f6097804a717032730e3fdc42e2bd8f6a63d
Jorge Sarricolea <jjorgesarricolea18@gmail.com>
2025-01-08 16:08:37 -0600

    feat(presenter): add UserPresenter
        - Added methods to format single users and user lists.
        - Ensures consistent output with fields like id, first_name, last_name, email, semester, is_tutor, and role_id.

---
+++ src/interfaces/presenters/UserPresenter.js
@@ -0,0 +1,19 @@
+class UserPresenter {
+  static format(user) {
+    return {
+      id: String(user.user_id),
+      first_name: user.first_name,
+      last_name: user.last_name,
+      email: user.email,
+      semester: user.semester,
+      is_tutor: user.is_tutor,
+      role_id: String(user.role_id),
+    };
+  }
+
+  static formatList(users) {
+    return users.map(UserPresenter.format);
+  }
+}
+
+export default UserPresenter;
