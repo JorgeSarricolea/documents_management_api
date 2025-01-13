92fcdfe4bd9a6e894e3536ba4204138d760a72cc
Jorge Sarricolea <jjorgesarricolea18@gmail.com>
2025-01-13 11:16:48 -0600

    feat(server): add routes for categories, document info, and user documents
        - Integrated CategoryRoutes under /api/v1/categories.
        - Added DocumentInfoRoutes under /api/v1/documents-info.
        - Included UserDocumentRoutes under /api/v1/user-documents.

--- src/infrastructure/server.js
+++ src/infrastructure/server.js
@@ -5,6 +5,9 @@ import errorHandler from "../interfaces/middlewares/error.handler.js";
 import AuthRoutes from "./routes/auth.routes.js";
 import RoleRoutes from "./routes/role.routes.js";
 import UserRoutes from "./routes/user.routes.js";
+import CategoryRoutes from "./routes/category.routes.js";
+import DocumentInfoRoutes from "./routes/documentInfo.routes.js";
+import UserDocumentRoutes from "./routes/userDocument.routes.js";
 
 const app = express();
 dotenv.config();
@@ -19,6 +22,9 @@ app.use(express.json());
 app.use(`${BASE_URL}/auth`, AuthRoutes);
 app.use(`${BASE_URL}/roles`, RoleRoutes);
 app.use(`${BASE_URL}/users`, UserRoutes);
+app.use(`${BASE_URL}/categories`, CategoryRoutes);
+app.use(`${BASE_URL}/documents-info`, DocumentInfoRoutes);
+app.use(`${BASE_URL}/user-documents`, UserDocumentRoutes);
 
 app.use(errorHandler);
 
